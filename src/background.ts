b95e7d810ef429bcb4687dd269628f48eccb2593
pokutuna <popopopopokutuna@gmail.com>
2015-01-22 22:45:34 +0900

    chrome://... 開いた時にエラーでてた

--- src/background.ts
+++ src/background.ts
@@ -2,7 +2,8 @@
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
    if (changeInfo.status === 'complete') {
-       // TODO check tab.url
+       // TODO check tab.url github
+       if (/^chrome\:\/\//.test(tab.url)) return
        chrome.tabs.executeScript(tabId, { file: 'js/content.js' })
        chrome.tabs.insertCSS(tabId, { file: 'css/content.css' })
     }
