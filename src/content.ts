a495f8cba594beb45dc87db6f6f1749b9d28c61d
pokutuna <popopopopokutuna@gmail.com>
2015-01-22 22:25:03 +0900

    delegate & extended interface

--- src/content.ts
+++ src/content.ts
@@ -1,6 +1,25 @@
 console.log('content')
 
-var el = document.createElement('div')
-el.classList.add('compato-tooltip')
-el.textContent = 'hoge'
-document.body.insertBefore(el, document.body.firstChild)
+interface DelegatedEvent extends Event {
+    delegateTarget?: HTMLElement
+}
+
+var delegate = function(
+    match: (HTMLElement) => boolean,
+    listener: (DelegatedEevent) => any
+) {
+    return function(event) {
+        var el = event.target;
+        do {
+            if (!match(el)) continue;
+            event.delegateTarget = el
+            listener.apply(this, arguments)
+        } while (el = el.parentNode)
+    }
+}
+
+document.body.addEventListener('mouseover', delegate(function(el) {
+    return el.nodeName === 'A' ? true : false
+}, function(event) {
+    console.log(event.delegateTarget)
+}))
