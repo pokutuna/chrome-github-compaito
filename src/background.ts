8ccd223d86b284c10e6004346a93a0788ebcf68d
pokutuna <popopopopokutuna@gmail.com>
2015-01-23 11:19:44 +0900

    chrome-devtool~ みたいなのもあった

--- src/background.ts
+++ src/background.ts
@@ -3,7 +3,7 @@
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
    if (changeInfo.status === 'complete') {
        // TODO check tab.url github
-       if (/^chrome\:\/\//.test(tab.url)) return
+       if (/^chrome/.test(tab.url)) return
        chrome.tabs.executeScript(tabId, { file: 'js/content.js' })
        chrome.tabs.insertCSS(tabId, { file: 'css/content.css' })
     }
