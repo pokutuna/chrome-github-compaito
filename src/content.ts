3a92c89686ea34bff9b89f19a93af0284be6565e
pokutuna <popopopopokutuna@gmail.com>
2015-02-02 03:42:47 +0900

    toriaezu

--- src/content.ts
+++ src/content.ts
@@ -3,16 +3,16 @@ console.log('content')
 module Compaito {
 
     export function init(): void {
-        var diffPicker = new RevisionPicker();
+        var revPickerr = new RevisionPicker();
 
         document.body.addEventListener('mouseover', Util.delegate(
             Util.isCommitUrlAnchorElement,
-            (event) => diffPicker.show(event.delegateTarget)
+            (event) => revPickerr.show(event.delegateTarget)
         ));
 
         document.body.addEventListener('mouseout', Util.delegate(
             Util.isCommitUrlAnchorElement,
-            (event) => setTimeout(() => { diffPicker.hide() }, 200)
+            (event) => setTimeout(() => { revPickerr.hide() }, 200)
         ));
     }
 
@@ -23,37 +23,63 @@ module Compaito {
 
     // classes
     export class RevisionPicker {
-        elem: HTMLButtonElement;
+        container: HTMLDivElement;
+        pickButton: HTMLButtonElement;
+        cancelButton: HTMLButtonElement;
+
         stickingElem: HTMLElement;
+        stickingRevision: string = '';
 
         constructor() {
-            this.elem = document.createElement('button');
-            this.elem.textContent = 'pick';
-            ['chrome-extension-compaito', 'diff-picker', 'none'].forEach(
-                (c: string) => this.elem.classList.add(c)
-            );
-            this.elem.addEventListener('click', (e) => this.onPickerClick(e));
-            document.body.appendChild(this.elem);
+            this.container = document.createElement('div');
+            Util.addClasses(this.container, ['container', 'none']);
+
+            this.pickButton = document.createElement('button');
+            this.pickButton.textContent = 'pick';
+            this.pickButton.addEventListener('click', (e) => this.onPick(e));
+            Util.addClasses(this.pickButton, ['pick-button']);
+
+            this.cancelButton = document.createElement('button');
+            this.cancelButton.textContent = 'x';
+            this.cancelButton.addEventListener('click', (e) => this.onCancel(e));
+            Util.addClasses(this.cancelButton, ['cancel-button', 'none']);
+
+            this.container.appendChild(this.pickButton);
+            this.container.appendChild(this.cancelButton);
+            document.body.appendChild(this.container);
         }
 
         show(relatedElem:HTMLElement) {
             this.stickingElem = relatedElem;
-            console.log(this.stickingElem)
             var rect = this.stickingElem.getBoundingClientRect();
-            this.elem.style.position = 'absolute';
-            this.elem.style.top  = window.pageYOffset + rect.top + 'px';
-            this.elem.style.left = window.pageXOffset + rect.right + 'px';
-            this.elem.classList.remove('none');
+            this.container.style.position = 'absolute';
+            this.container.style.top  = window.pageYOffset + rect.top + 'px';
+            this.container.style.left = window.pageXOffset + rect.right + 'px';
+            this.container.classList.remove('none');
         }
 
         hide() {
-            this.elem.classList.add('none');
+            this.container.classList.add('none');
         }
 
-        onPickerClick(event: MouseEvent) {
+        onPick(event: MouseEvent) {
             var a = <HTMLAnchorElement> this.stickingElem;
-            var match = a.href.match(Util.commitUrlPattern);
-            this.elem.textContent = Util.abbRevision(match[1]);
+            var currentRev = this.stickingRevision;
+            this.stickingRevision = Util.extractRevision(a.href);
+            if (currentRev.length > 0) this.openCompareView(currentRev, this.stickingRevision);
+            this.pickButton.textContent = Util.abbRevision(this.stickingRevision) + '...';
+            this.cancelButton.classList.remove('none');
+        }
+
+        onCancel(event: MouseEvent) {
+            this.pickButton.textContent = 'pick';
+            this.cancelButton.classList.add('none');
+            this.stickingRevision = '';
+            this.hide();
+        }
+
+        openCompareView(from, to: string) {
+            location.pathname = Util.constructCompareViewPath(from, to);
         }
     }
 
@@ -71,15 +97,31 @@ module Compaito {
             }
         }
 
+        export function addClasses(elem: HTMLElement, classes: string[]): void {
+            elem.classList.add('chrome-extension-compaito');
+            classes.forEach((c: string) => elem.classList.add(c));
+        }
+
+        // Github
         export var commitUrlPattern: RegExp = /\/commit\/([0-9a-f]{40})/;
         export function isCommitUrlAnchorElement(elem: HTMLElement): boolean {
             var a = <HTMLAnchorElement> elem;
             return a.nodeName === 'A' && commitUrlPattern.test(a.href) ? true : false;
         }
-
+        export function extractRevision(url: string): string {
+            var match = url.match(commitUrlPattern);
+            return match[1];
+        }
+        export function constructCompareViewPath(fromRev, toRev: string): string {
+            var diffArg = [fromRev, toRev].join('...');
+            var pattern: RegExp = /\/([^\/]+)\/([^\/]+)\/.*/;
+            var match = location.pathname.match(pattern);
+            return [match[1], match[2], 'compare', diffArg].join('/');
+        }
         export function abbRevision(revision: string): string {
             return revision.substring(0, 7) // follow Github abbreviation
         }
+
     }
 }
 
