b8b6b66294f97c229c9f546ce8a69989e23e8411
pokutuna <popopopopokutuna@gmail.com>
2015-02-02 02:51:55 +0900

    iroiro

--- src/content.ts
+++ src/content.ts
@@ -3,7 +3,7 @@ console.log('content')
 module Compaito {
 
     export function init(): void {
-        var diffPicker = new DiffPicker();
+        var diffPicker = new RevisionPicker();
 
         document.body.addEventListener('mouseover', Util.delegate(
             Util.isCommitUrlAnchorElement,
@@ -12,7 +12,7 @@ module Compaito {
 
         document.body.addEventListener('mouseout', Util.delegate(
             Util.isCommitUrlAnchorElement,
-            (event) => setTimeout(() => { diffPicker.hide() }, 100)
+            (event) => setTimeout(() => { diffPicker.hide() }, 200)
         ));
     }
 
@@ -22,27 +22,38 @@ module Compaito {
     }
 
     // classes
-    export class DiffPicker {
+    export class RevisionPicker {
         elem: HTMLButtonElement;
+        stickingElem: HTMLElement;
 
         constructor() {
             this.elem = document.createElement('button');
+            this.elem.textContent = 'pick';
             ['chrome-extension-compaito', 'diff-picker', 'none'].forEach(
                 (c: string) => this.elem.classList.add(c)
             );
+            this.elem.addEventListener('click', (e) => this.onPickerClick(e));
             document.body.appendChild(this.elem);
         }
 
         show(relatedElem:HTMLElement) {
-            var rect = relatedElem.getBoundingClientRect();
+            this.stickingElem = relatedElem;
+            console.log(this.stickingElem)
+            var rect = this.stickingElem.getBoundingClientRect();
             this.elem.style.position = 'absolute';
-            this.elem.style.top  = rect.top   + 'px';
-            this.elem.style.left = rect.right + 'px';
+            this.elem.style.top  = window.pageYOffset + rect.top + 'px';
+            this.elem.style.left = window.pageXOffset + rect.right + 'px';
             this.elem.classList.remove('none');
         }
 
         hide() {
             this.elem.classList.add('none');
+        }
+
+        onPickerClick(event: MouseEvent) {
+            var a = <HTMLAnchorElement> this.stickingElem;
+            var match = a.href.match(Util.commitUrlPattern);
+            this.elem.textContent = Util.abbRevision(match[1]);
         }
     }
 
@@ -60,10 +71,14 @@ module Compaito {
             }
         }
 
-        var commitUrlPattern: RegExp = /\/commit\/[0-9a-f]{40}/;
+        export var commitUrlPattern: RegExp = /\/commit\/([0-9a-f]{40})/;
         export function isCommitUrlAnchorElement(elem: HTMLElement): boolean {
             var a = <HTMLAnchorElement> elem;
             return a.nodeName === 'A' && commitUrlPattern.test(a.href) ? true : false;
+        }
+
+        export function abbRevision(revision: string): string {
+            return revision.substring(0, 7) // follow Github abbreviation
         }
     }
 }
