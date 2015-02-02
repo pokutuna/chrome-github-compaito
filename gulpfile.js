ec24e2bcbc8501cca2a3c1ff903d92da75d24469
pokutuna <popopopopokutuna@gmail.com>
2015-02-03 04:17:25 +0900

    to app directory

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,26 @@
-var gulp = require('gulp'),
+var gulp    = require('gulp'),
     changed = require('gulp-changed'),
-    tsc  = require('gulp-tsc');
+    tsc     = require('gulp-tsc'),
+    jade    = require('gulp-jade');
 
 gulp.task('typescript', function() {
     gulp.src('src/**/*.ts')
         .pipe(changed('src/js'), { extension: '.js' })
         .pipe(tsc({ emitError: false }))
-        .pipe(gulp.dest('src/js'));
+        .pipe(gulp.dest('app/js'));
 });
+
+// TODO less
+gulp.task('css', function() {
+    gulp.src('src/css/*.css')
+        .pipe(gulp.dest('app/css'));
+});
+
+gulp.task('jade', function() {
+    gulp.src('src/jade/**/*.jade')
+        .pipe(jade())
+        .pipe(gulp.dest('app/html'));
+})
 
 gulp.task('watch', function() {
     gulp.watch('src/**/*.ts', ['typescript']);
