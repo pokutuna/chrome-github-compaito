91c90be7b016034c57fe91ba96b1b465174b0bb6
pokutuna <popopopopokutuna@gmail.com>
2015-02-03 19:31:35 +0900

    version

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
-var gulp    = require('gulp'),
-    changed = require('gulp-changed'),
-    tsc     = require('gulp-tsc'),
-    sass    = require('gulp-sass'),
-    jade    = require('gulp-jade');
+var gulp     = require('gulp'),
+    changed  = require('gulp-changed'),
+    editJson = require('gulp-json-editor'),
+    tsc      = require('gulp-tsc'),
+    sass     = require('gulp-sass'),
+    jade     = require('gulp-jade'),
+    exec     = require('child_process').exec,
+    Promise  = require('es6-promise').Promise;
 
 gulp.task('default', ['build', 'watch']);
 
@@ -27,9 +30,29 @@ gulp.task('jade', function() {
 
 // TODO fill-in version
 gulp.task('manifest', function() {
-    gulp.src('src/manifest.json')
+    version().then(function(version) {
+        gulp.src('src/manifest.json')
+        .pipe(editJson({version : version }))
         .pipe(gulp.dest('app/'));
+    });
 });
+
+function version() {
+    var promise = new Promise(function(resolve, reject) {
+        exec('git describe --tags --always --dirty', function(err, stdout, stderr) {
+            if (err) {
+                reject(err);
+                return;
+            }
+            var version = stdout.replace(/\n$/, '')
+                    .replace(/-(\d+)/, '.$1')
+                    .replace(/-g[0-9a-f]+/, '')
+                    .replace(/-dirty/, '');
+            resolve(version);
+        });
+    });
+    return promise;
+}
 
 gulp.task('build', ['manifest', 'typescript', 'sass', 'jade']);
 
