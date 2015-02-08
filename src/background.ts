f0c5db903a0d7b3eb8ab0092fb95e2d8fa9ba1b9
pokutuna <popopopopokutuna@gmail.com>
2015-02-08 22:28:00 +0900

    check config host

--- src/background.ts
+++ src/background.ts
@@ -1,10 +1,23 @@
 /// <reference path="../modules/DefinitelyTyped/chrome/chrome.d.ts" />
+import common = require('./common');
+
+function isHostEnabled(url: string, config: common.CompaitoConfig): boolean {
+    var match = /^\w+:\/\/([^\/]+)/.exec(url);
+    if (!match) return false;
+    var host = match[1];
+    return Object.keys(config.hosts).some(function(key) {
+        var pattern = new RegExp('^' + key.replace(/\*/g, '.+?') + '$');
+        return pattern.test(host);
+    });
+}
 
 chrome.tabs.onUpdated.addListener(function (tabId: number, changeInfo: chrome.tabs.TabChangeInfo, tab: chrome.tabs.Tab) {
    if (changeInfo.status === 'complete') {
-       // TODO check tab.url github
-       if (/^chrome/.test(tab.url)) return
-       chrome.tabs.executeScript(tabId, { file: 'js/content.js' })
-       chrome.tabs.insertCSS(tabId, { file: 'css/content.css' })
+       chrome.storage.sync.get(common.DEFAULT_CONFIG, (config: common.CompaitoConfig) => {
+           if (isHostEnabled(tab.url, config)) {
+               chrome.tabs.executeScript(tabId, { file: 'js/content.js' })
+               chrome.tabs.insertCSS(tabId, { file: 'css/content.css' })
+           }
+       });
     }
 })
