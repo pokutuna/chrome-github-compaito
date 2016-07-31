0bd6b1d691de9032bc96813bbcfecaee8330af54
pokutuna <popopopopokutuna@gmail.com>
2016-08-01 05:03:52 +0900

    component

--- src/components/CommitPicker.ts
+++ src/components/CommitPicker.ts
@@ -1,25 +1,47 @@
 import { IView, Presenter, View } from './base';
 import { Util } from './Util';
+import { CommitURL } from './GitHub';
 
 interface ICommitPickerView extends IView {
+    showPicker(): void;
+    hidePicker(): void;
 }
 
 class CommitPickerPresenter extends Presenter {
     view: ICommitPickerView;
 
+    hoveringCommit: CommitURL;
+    fromCommit: CommitURL;
+
     constructor(view: ICommitPickerView) {
         super(view);
+        this.hoveringCommit = null;
+        this.fromCommit = null;
+    }
+
+    handleMouseOver(url: string): void {
+        if (!CommitURL.isCommitURLString(url)) return;
+        this.hoveringCommit = new CommitURL(url);
+        this.view.showPicker();
+    }
+
+    handleMouseOut(url: string): void {
+        this.hoveringCommit = null;
+        this.view.hidePicker();
     }
 }
 
 class CommitPickerView extends View implements ICommitPickerView {
     presenter: CommitPickerPresenter;
 
+    container: HTMLBodyElement;
+
     constructor(body: HTMLBodyElement) {
         super(body);
     }
 
     initMembers(body: HTMLBodyElement) {
+        this.container = body;
     }
 
     createPresenter() {
@@ -27,5 +49,27 @@ class CommitPickerView extends View implements ICommitPickerView {
     }
 
     registerEvents(): void {
+        this.container.addEventListener(
+            'mouseover', Util.delegate('a', this.onAnchorMouseOver.bind(this)));
+
+        this.container.addEventListener(
+            'mouseout', Util.delegate('a', this.onAnchorMouseOut.bind(this))
+        );
+    }
+
+    private onAnchorMouseOver(event: Event): void {
+        if ((<HTMLElement>event.target).nodeName !== 'A') return;
+        this.presenter.handleMouseOver((<HTMLAnchorElement>event.target).href);
+    }
+
+    private onAnchorMouseOut(event: Event): void {
+        if ((<HTMLElement>event.target).nodeName !== 'A') return;
+        this.presenter.handleMouseOut((<HTMLAnchorElement>event.target).href);
+    }
+
+    showPicker(): void {
+    }
+
+    hidePicker(): void {
     }
 }
--- src/components/GitHub.ts
+++ src/components/GitHub.ts
@@ -1,30 +1,21 @@
-class GitHub {
-    static isCommitUrlAnchorElement(elem: HTMLElement): boolean {
-        const a = <HTMLAnchorElement> elem;
-        return a.nodeName === 'A' &&
-            this.commitUrlPattern.test(a.href) &&
-            !/#$/.test(a.href) ? true : false;
-     }
-
-    static constructCompareViewUrl(fromRev: string, toRev: string): string {
-        var diffArg = [fromRev, toRev].join('...');
-        var pattern: RegExp = /\/([^\/]+)\/([^\/]+)(\/.*)?/;
-        var match = location.pathname.match(pattern);
-        return [location.origin, match[1], match[2], 'compare', diffArg].join('/');
-    }
-}
-
 class CommitURL {
     url: string;
-    isRefParent: boolean
+    _parts: string[];
+    isRefParent: boolean;
 
     constructor(url: string, isRefParent: boolean = false) {
-        this.url          = url;
+        this.url = url;
+
+        // ["{schema}:", "", "{host}", "{owner}", "{repo}", "commit", "{sha1}"]
+        //   or
+        // ["{schema}:", "", "{host}", "{owner}", "{repo}", "pull", "{num}", "commits", "{sha1}"]
+        this._parts = this.url.split('/');
+
         this.isRefParent = isRefParent;
     }
 
     get revision(): string {
-        const match = this.url.match(GitHub.commitUrlPattern);
+        const match = this.url.match(CommitURL.pattern);
         return this.isRefParent ? match[1] : match[1] + '~';
     }
 
@@ -36,10 +27,43 @@ class CommitURL {
     }
 
     get isInPullRequest(): boolean {
-        return false; // TODO
+        return this._parts[5] === 'pull' ? true : false;
+    }
+
+    get owner(): string {
+        return this._parts[3];
+    }
+
+    get repo(): string {
+        return this._parts[4];
+    }
+
+    get issue(): string {
+        return this._parts[6];
     }
 
     static pattern: RegExp = /\/commits?\/([0-9a-f]{40})/;
+
+    static isCommitURLString(url: string): boolean {
+        return this.pattern.test(url) && !/#$/.test(url) ? true : false;
+    }
+
+    static composeDiffURL(from: CommitURL, to: CommitURL): string {
+
+        // /{org}/{repo}/pull/{issue}/files/{from}..{to}
+        if (from.isInPullRequest && to.isInPullRequest &&
+            // Check same repository
+            from.owner === to.owner && from.repo === to.repo &&
+            // Cannot use `~` in PR compare view
+            !from.isRefParent && !to.isRefParent) {
+            const pr = from._parts.slice(0, 7).join('/');
+            return `${pr}/files/${from.revision}..${to.revision}`;
+        }
+
+        // /{org}/{repo}/compare/{from}...{to}
+        const repo = from._parts.slice(0, 5).join('/');
+        return `${repo}/compare/${from.revision}...${to.revision}`;
+    }
 }
 
-export { GitHub, CommitURL };
+export { CommitURL };
