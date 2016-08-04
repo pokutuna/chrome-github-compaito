99b95417fa505810be34d4dd0918a78dfb6f1738
pokutuna <popopopopokutuna@gmail.com>
2016-08-04 12:16:31 +0900

    add sinon

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,7 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha', 'browserify', 'chai'],
+    frameworks: ['mocha', 'browserify', 'chai', 'sinon'],
 
 
     // list of files / patterns to load in the browser
