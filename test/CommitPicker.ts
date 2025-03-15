1fa54e411f411d9a1c92eb0a41f0541edb5ca7b3
shibayu36 <shibayu36@gmail.com>
2025-03-15 14:49:16 +0900

    Change to use the @types/ format so that npm run build runs successfully

--- test/CommitPicker.ts
+++ test/CommitPicker.ts
@@ -1,4 +1,5 @@
 /// <reference path="test.d.ts" />
+import * as sinon from 'sinon';
 import {
     ICommitPickerView,
     CommitPickerPresenter,
--- test/CompaitoConfig.ts
+++ test/CompaitoConfig.ts
@@ -1,4 +1,5 @@
 /// <reference path="test.d.ts" />
+import * as sinon from 'sinon';
 import { CompaitoConfig } from '../src/components/CompaitoConfig';
 
 describe('CompaitoConfig', () => {
